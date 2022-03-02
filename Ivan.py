2d39d1a454714e33111a1ebc481c547c30412494
ivansito18 <100377629+ivansito18@users.noreply.github.com>
2022-03-01 22:20:25 -0600

    Update Ivan.py

--- Ivan.py
+++ Ivan.py
@@ -1,13 +1,14 @@
-dna = TUUUCCCUCGA
+dna="GATGGAACTTGACTACGTAAATT"
 def dna2rna(dna):
-  rna=''
-  if symbol == 'T':
-    rna = rna + 'U'
-  elif symbol == 'U':
-    rna += 'U'
-  elif symbol == 'A':
-    rna += 'A'
-  elif symbol == 'C':
-    rna += 'C'
-  return rna
-print(dna)
+    rna = ''
+    for symbol in dna:
+        if symbol == 'A':
+            rna = rna + 'A'
+        elif symbol == 'C':
+            rna += 'C'
+        elif symbol == 'G':
+            rna += 'G'
+        elif symbol == 'T':
+            rna += 'U'
+    return rna
+print(dna2rna(dna))
