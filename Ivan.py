875249461ce8119f691991d407e406ed98712971
ivansito18 <100377629+ivansito18@users.noreply.github.com>
2022-03-03 11:01:43 -0600

    Update Ivan.py

--- Ivan.py
+++ Ivan.py
@@ -13,5 +13,23 @@ def dna2rna(dna):
     return rna
 print(dna2rna(dna))
 
+a=1
+b=0
+c=0
+d=1
+e=0
+f=1
+couple = [a,b,c,d,e,f]
+def count_dom_phenotype(couple):
+    return (((2 * a) + (2 * b) + (2 * c) + ((3/2) * d)+ e + (0 * f)))
+def genotype():   
+    a = int(couple[0])
+    b = int(couple[1])
+    c = int(couple[2])
+    d = int(couple[3])
+    e = int(couple[4])
+    f = int(couple[5])
+print(count_dom_phenotype(couple))
+
 
 
