1a8d23a63e6b0054d5a3ac84f6efc2ee4cb9cc1c
jaymp2 <jaymp2@illinois.edu>
2022-03-04 16:09:35 -0600

    final funcations

--- JayP.py
+++ JayP.py
@@ -31,7 +31,47 @@ def s(dna):
     return count 
 
 
-def locate_substring(dna_snippet, dna):
-    if dna_snippet in dna:
+def hamming_dist(dna1,dna2):
+    value = 0
+    for i in range(len(dna1)):
+        if dna1[i] != dna2[i]:
+            value += 1
+    return value 
+
+
+def locate_substring(dna_substring, dna):
+    result = -1
+    first_loop = True
+    locations = []
+    while (result != -1 or first_loop):
+        first_loop = False
+        result = dna.find(dna_substring, result + 1)
+        if(result != -1):
+            locations.append(result)
+    return locations
+   
+
+table = {'UUU':'F', 'UUC':'F', 'UUA':'L', 'UUG':'L', 'UCU':'S', 'UCC':'S', 'UCA':'S', 'UCG':'S', 'UAU':'Y',
+     'UAC':'Y', 'UAA':'Stop', 'UAG':'Stop', 'UGU':'C', 'UGC':'C', 'UGA':'Stop', 'UGG':'W', 'CUU':'L',
+     'CUC':'L', 'CUA':'L', 'CUG':'L', 'CCU':'P', 'CCC':'P', 'CCA':'P', 'CCG':'P', 'CAU':'H', 'CAC':'H',
+     'CAA':'Q', 'CAG':'Q', 'CGU':'R', 'CGC':'R', 'CGA':'R', 'CGG':'R', 'AUU':'I', 'AUC':'I', 'AUA':'I',
+     'AUG':'M', 'ACU':'T', 'ACC':'T', 'ACA':'T', 'ACG':'T', 'AAU':'N', 'AAC':'N', 'AAA':'K', 'AAG':'K',
+     'AGU':'S', 'AGC':'S', 'AGA':'R', 'AGG':'R', 'GUU':'V', 'GUC':'V', 'GUA':'V', 'GUG':'V', 'GCU':'A',
+     'GCC':'A', 'GCA':'A', 'GCG':'A', 'GAU':'D', 'GAC':'D', 'GAA':'E', 'GAG':'E', 'GGU':'G', 'GGC':'G',
+     'GGA':'G', 'GGG':'G'}
+table_values = list(table.values())
+def source_rna(protein):
+    count = 1
+    for i in protein:
+        count *= table_values.count(i)
+    count *= table_values.count('Stop')
+    return count
+
+
         
-    
+
+        
+
+        
+        
+        
