3ef6faefa8113b9374130df5e4ff24eceda8b189
rickyjh47 <100377805+rickyjh47@users.noreply.github.com>
2022-03-04 16:22:33 -0600

    Update Ivan.py

--- Ivan.py
+++ Ivan.py
@@ -64,7 +64,21 @@ def gc_content(dna_list):
     max_index = gc_content_list.index(highest_gc)
     return max_index, highest_gc
 
+#rna2codon
 
+table = {
+        'UUU': 'F', 'UUC': 'F', 'UUA': 'L', 'UUG': 'L',        'CUU': 'L', 'CUC': 'L', 'CUA': 'L', 'CUG': 'L',
+        'AUU': 'I', 'AUC': 'I', 'AUA': 'I', 'AUG': 'M',        'GUU': 'V', 'GUC': 'V', 'GUA': 'V', 'GUG': 'V',
+
+        'UCU': 'S', 'UCC': 'S', 'UCA': 'S', 'UCG': 'S',        'CCU': 'P', 'CCC': 'P', 'CCA': 'P', 'CCG': 'P',
+        'ACU': 'T', 'ACC': 'T', 'ACA': 'T', 'ACG': 'T',        'GCU': 'A', 'GCC': 'A', 'GCA': 'A', 'GCG': 'A',
+
+        'UAU': 'Y', 'UAC': 'Y', 'UAA': '*', 'UAG': '*',        'CAU': 'H', 'CAC': 'H', 'CAA': 'Q', 'CAG': 'Q',
+        'AAU': 'N', 'AAC': 'N', 'AAA': 'K', 'AAG': 'K',        'GAU': 'D', 'GAC': 'D', 'GAA': 'E', 'GAG': 'E',
+
+        'UGU': 'C', 'UGC': 'C', 'UGA': '*', 'UGG': 'W',        'CGU': 'R', 'CGC': 'R', 'CGA': 'R', 'CGG': 'R',
+        'AGU': 'S', 'AGC': 'S', 'AGA': 'R', 'AGG': 'R',        'GGU': 'G', 'GGC': 'G', 'GGA': 'G', 'GGG': 'G',
+    }
 def rna2codon(rna):
     def triplet2codon(triplet):
         allowed_codons = set('AGCU')
@@ -83,3 +97,20 @@ string = "AUGGCCAUGGCGCCCAGAACUGAGAUCAAUAGUACCCGUAUUAACGGGUGA"
 print(rna2codon(string))
 
 
+
+#reverse_compliment
+
+def reverse_complement(dna):
+    dna = dna[::-1]
+    ans = ""
+    for ch in dna:
+        if ch == "A":
+            ans += "T"
+        elif ch == "C":
+            ans += "G"
+        elif ch == "T":
+            ans += "A"
+        elif ch == "G":
+            ans += "C"
+
+    return ans
